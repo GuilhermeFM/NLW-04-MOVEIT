562f6a1fcbfaa50e85e4564b309dc3a9769f172a
Guilherme Frizzera Meira <guifrizzera2005@gmail.com>
2021-02-28 15:48:54 -0300

    Fix: login layout for mobile.

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -7,39 +7,43 @@ export default function login() {
   const [username, setUsername] = useState<string>();
 
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Login | move.it</title>
-      </Head>
+    <div className={styles.background}>
+      <div className={styles.container}>
+        <Head>
+          <title>Login | move.it</title>
+        </Head>
 
-      <img src="/icons/background-logo.svg" alt="background" />
+        <div className={styles.containerLeft}>
+          <img src="/icons/background-logo.svg" alt="background" />
+        </div>
 
-      <div>
-        <header className={styles.containerHeader}>
-          <img src="/icons/logo.svg" alt="move it" />
-          <strong>Bem Vindo</strong>
-        </header>
+        <div className={styles.containerRight}>
+          <header>
+            <img src="/icons/logo.svg" alt="move it" />
+            <strong>Bem Vindo</strong>
+          </header>
 
-        <main className={styles.containerMain}>
-          <div>
-            <img src="/icons/github-logo.svg" alt="github-logo" />
-            <p>Faça login com seu github para começar</p>
-          </div>
-          <div>
-            <input
-              type="text"
-              placeholder="Digite seu username"
-              value={username}
-              onChange={e => setUsername(e.target.value)}
-            />
-            <button
-              type="button"
-              className={`${username && styles.buttonHighligh}`}
-            >
-              <img src="/icons/arrow-right.svg" alt="logar" />
-            </button>
-          </div>
-        </main>
+          <main className={styles.main}>
+            <div>
+              <img src="/icons/github-logo.svg" alt="github-logo" />
+              <p>Faça login com seu github para começar</p>
+            </div>
+            <div>
+              <input
+                type="text"
+                placeholder="Digite seu username"
+                value={username}
+                onChange={e => setUsername(e.target.value)}
+              />
+              <button
+                type="button"
+                className={`${username && styles.buttonHighlight}`}
+              >
+                <img src="/icons/arrow-right.svg" alt="logar" />
+              </button>
+            </div>
+          </main>
+        </div>
       </div>
     </div>
   );
