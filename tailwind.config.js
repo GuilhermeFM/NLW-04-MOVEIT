06723945fa6e49a26d3a2cd4c5f35ca140f10a1b
Guilherme Frizzera Meira <guifrizzera2005@gmail.com>
2021-03-06 17:58:00 -0300

    Something-again.....

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,6 @@
 const colors = require('tailwindcss/colors');
 
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-
   theme: {
     colors: {
       ...colors,
