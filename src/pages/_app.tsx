be4ec7ca9c4b97e171551d946383c2275148f562
Guilherme Frizzera Meira <guifrizzera2005@gmail.com>
2021-03-08 12:05:27 -0300

    Initial

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,50 @@
-import '../styles/global.css';
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
+import { createBreakpoints } from '@chakra-ui/theme-tools';
+
+const theme = extendTheme({
+  colors: {
+    brand: {
+      background: '#f2f3f5',
+      grayLine: '#dcdde0',
+      text: '#666666',
+      textHighlight: '#b3b9ff',
+      title: '#2e384d',
+
+      red: '#e83f5b',
+      green: '#4cd62b',
+      blue100: '#b3b9ff',
+      blue200: '#5965e0',
+      blue300: '#4953b8',
+      gray100: '#f0f1f3',
+      white: '#ffffff',
+    },
+  },
+  styles: {
+    global: {
+      'html, body': {
+        margin: '0',
+        padding: '0',
+        width: '100vw',
+        height: '100vh',
+      },
+    },
+  },
+  breakpoints: createBreakpoints({
+    sm: '640px',
+    md: '768px',
+    lg: '1024px',
+    xl: '1280px',
+  }),
+});
+
+// import '../styles/global.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
 
 export default MyApp;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,41 +1,19 @@
 import axios from 'axios';
 import Head from 'next/head';
-import { useState, useRef } from 'react';
 import { GetServerSideProps } from 'next';
 
+import {
+  Flex,
+  Center,
+  Box,
+  Image,
+  Text,
+  VStack,
+  HStack,
+  chakra,
+} from '@chakra-ui/react';
+
 import * as Icons from '../components/Icons';
-
-interface InputTextWithButtonProps {
-  onClick(value: string): void;
-}
-
-function InputTextWithButton({ onClick }: InputTextWithButtonProps) {
-  const ref = useRef<HTMLInputElement>();
-
-  function handleOnClick() {
-    const value = ref.current.value;
-    if (value) onClick(value);
-  }
-
-  return (
-    <div className="app-sign-in-form-input">
-      <input
-        ref={ref}
-        type="text"
-        autoComplete="off"
-        placeholder="Digite seu username"
-        className="app-sign-in-form-input-control"
-      />
-      <button
-        type="submit"
-        onClick={handleOnClick}
-        className={`app-sign-in-form-input-button hover:bg-green-500`}
-      >
-        <Icons.Forward className="app-sign-in-form-input-button-img" />
-      </button>
-    </div>
-  );
-}
 
 export default function SignIn() {
   async function handleOnClick(value: string) {
@@ -45,27 +23,109 @@ export default function SignIn() {
   }
 
   return (
-    <div className="app-sign-in">
-      <div className="app-sign-in-main">
-        <Head>
-          <title>Login | move.it</title>
-        </Head>
+    <>
+      <Head>
+        <title>Login | move.it</title>
+      </Head>
 
-        <img className="app-sign-in-logo" src="/logos/logo-background.svg" alt="background" />
+      <Flex
+        width="100vw"
+        height="100vh"
+        overflow="auto"
+        direction="row"
+        alignItems="center"
+        justifyContent="center"
+        backgroundColor="brand.blue200"
+        padding={['5', '10']}
+      >
+        <Flex
+          width="50%"
+          height="100%"
+          direction="row"
+          alignItems="center"
+          justifyContent="center"
+          display={{ base: 'none', xl: 'initial' }}
+        >
+          <Image
+            width="100%"
+            height="100%"
+            alt="background"
+            src="/logos/logo-background.svg"
+            paddingY="50"
+          />
+        </Flex>
 
-        <div className="app-sign-in-form">
-          <section className="app-sign-in-form-brand">
-            <img src="/logos/logo-text.svg" alt="move it" />
-            <p className="app-sign-in-form-brand-text">Bem Vindo</p>
-          </section>
-          <section className="app-sign-in-form-github">
-            <img src="/logos/logo-github.svg" alt="github-logo" />
-            <p className="app-sign-in-form-github-text">Faça login com seu github para começar</p>
-          </section>
-          <InputTextWithButton onClick={handleOnClick} />
-        </div>
-      </div>
-    </div>
+        <Center width={{ base: '100%', xl: '50%' }}>
+          <VStack
+            width={{ base: '100%', xl: 'xl' }}
+            spacing="9"
+            align="flex-start"
+          >
+            <VStack spacing="5" align="flex-start">
+              <Image src="/logos/logo-text.svg" alt="move it" />
+              <Text
+                textColor="brand.white"
+                fontFamily="Inter"
+                fontWeight="600"
+                fontSize={{ base: '', xl: '2xl' }}
+                marginTop="10"
+              >
+                Bem Vindo
+              </Text>
+            </VStack>
+
+            <HStack spacing="5" align="center">
+              <Image src="/logos/logo-github.svg" alt="github-logo" />
+              <Text
+                textColor="brand.blue100"
+                fontFamily="Inter"
+                fontWeight="500"
+                fontSize={['sm', 'xl']}
+              >
+                Faça login com seu github para começar
+              </Text>
+            </HStack>
+
+            <HStack width="100%" spacing="0">
+              <chakra.input
+                flex="1"
+                height={{ base: '12', md: '16', xl: '20' }}
+                outline="none"
+                paddingX="4"
+                color="brand.blue100"
+                fontFamily="Inter"
+                fontSize={['sm', 'md', 'xl']}
+                fontWeight="medium"
+                borderStartRadius={{ base: 'md', md: 'lg', xl: 'xl' }}
+                backgroundColor="transparent"
+                bgGradient="linear(to-r, brand.blue300, brand.blue200);"
+                placeholder="Username or email"
+              />
+              <chakra.button
+                width={{ base: '12', md: '16', xl: '20' }}
+                height={{ base: '12', md: '16', xl: '20' }}
+                border="none"
+                borderEndRadius={{ base: 'md', md: 'lg', xl: 'xl' }}
+                backgroundColor="brand.blue300"
+                outline="none"
+                transition="all"
+                transitionDuration="0.3s"
+                _hover={{ backgroundColor: 'brand.green' }}
+              >
+                <Center>
+                  <Box
+                    width={{ base: '8', md: '12', xl: '16' }}
+                    height={{ base: '8', md: '12', xl: '16' }}
+                  >
+                    <Icons.Play width="100%" height="100%" />
+                  </Box>
+                </Center>
+              </chakra.button>
+            </HStack>
+          </VStack>
+        </Center>
+      </Flex>
+    </>
   );
 }
 
